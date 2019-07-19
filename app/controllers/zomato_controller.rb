acbf2c54f56326959d4915f70d6b4a3b0ac0a7c3
bebopvinh <vinhdo07@gmail.com>
2019-07-18 19:10:29 -0700

    Parse location from params to get correct coordinates for get request

--- app/controllers/zomato_controller.rb
+++ app/controllers/zomato_controller.rb
@@ -1,20 +1,21 @@
 class ZomatoController < ApplicationController
-   def index
+   def create
       conn = Faraday.new(
          url: 'https://developers.zomato.com/api/v2.1/geocode',
-         # params: {
-         #    lat: 34.052235,
-         #    lon: -118.243683
-         # },
+
          headers: {
             'Content-Type': 'application/json',
             'user-key': 'ede3852ed1c7062dab6b55dd8cff2417'
          }
       )  
 
+      coordinates = params[:zomato][:location].split(",")
+      latitude = coordinates.first.strip.to_f
+      longitude = coordinates.last.strip.to_f
+      
       resp = conn.get('') do |req|
-         req.params['lat'] = 34.052235
-         req.params['lon'] = -118.243683
+         req.params['lat'] = latitude
+         req.params['lon'] = longitude
       end
 
       render json: JSON.parse(resp.body)
