45152a7d7d150552747995c5bbbbc9968462d509
BebopVinh <vinhdo07@gmail.com>
2019-07-25 00:25:31 -0700

    Add routes to create/destroy sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:create]
 
-  # get '/restaurants', to: 'restaurants#restaurants'
+  resources :sessions, only: [:create, :destroy]
+
+  resources :recipes, only: [:create]
   resources :restaurants, only: [:create]
 end
