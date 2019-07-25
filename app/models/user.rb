4a69d57422db9ae097505ae75098f06cf76e831a
BebopVinh <vinhdo07@gmail.com>
2019-07-25 00:25:51 -0700

    initial files for devise generation

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  #For simple_token_authentication
+  acts_as_token_authenticatable
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
