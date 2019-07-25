db3d602b5930721e37e9cb6c3872f1fb3e488df6
BebopVinh <vinhdo07@gmail.com>
2019-07-25 12:57:28 -0700

    Add UsersController for signing up and setting allergen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
+
    def set_allergen
-      
+      user = User.find_by(id: params[:user][:user_id])
+      user.update(allergen: params[:user][:allergen])
+      render json: user.as_json(only: :allergen), status: :ok
    end
+
+   def sign_up
+      user = User.new(user_params)
+      if user.valid?
+         user.save
+         render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
+      else
+         head(:bad_request)
+      end
+   end
+
+
+   private
+      def user_params
+         params.require(:user).permit(:email, :password, :password_confirmation)
+      end
 end
