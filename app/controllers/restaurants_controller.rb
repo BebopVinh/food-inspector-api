4c5bdc13b7610070ace50f36520da8c7553a3aa6
BebopVinh <vinhdo07@gmail.com>
2019-10-11 22:10:20 -0700

    Removed exposed API key and modified lat/lon variables

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,16 @@
 class RestaurantsController < ApplicationController
    def create
-      coords = get_coordinates(params[:location])
       url = URI.parse("https://us-restaurant-menus.p.rapidapi.com")
       resp = Faraday.get(url + "/restaurants/search") do |req|
          req.headers['X-RapidAPI-Host'] = "us-restaurant-menus.p.rapidapi.com"
-         req.headers['X-RapidAPI-Key'] = "a65e0a6ec9mshe629e4a46efeb13p1f5432jsn0b66f46daafb"
-         req.params["lat"] = coords["lat"]
-         req.params["lon"] = coords["lng"]
-         req.params["distance"] = 5
+         req.headers['X-RapidAPI-Key'] = ENV["RapidAPI_KEY"]
+         req.params["lat"] = params["lat"]
+         req.params["lon"] = params["lng"]
+         req.params["distance"] = 10
          req.params["fullmenu"] = true
       end
 
       json = JSON.parse(resp.body)
-      restaurants = Restaurant.clean_results(json)
-      
-      render json: restaurants
-      
+      render json: json
    end
 end
